572073332c2155f4139c517f9d73697ff7d0ef44
dwg7486 <dwg7486@rit.edu>
2020-02-09 03:08:33 -0500

    Including more players

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const WIDTH = 720;
 const HEIGHT = 720;
 const TURN_SPEED = 180;
+const MAX_PLAYERS = 4;
+const PLAYER_OFFSET = 50;
 
 var config = {
     type: Phaser.AUTO,
@@ -21,23 +23,24 @@ var config = {
 
 var game = new Phaser.Game(config);
 var cursors;
-var player;
+var players = [];
 
 function preload ()
 {
     this.load.setBaseURL('http://localhost:8000/');
 
     this.load.image('bg', 'assets/img/background.jpg');
-    this.load.image('brick_01', 'assets/img/brick_01.png');
-    this.load.image('player', 'assets/img/Player1.png');
+    this.load.image('brick_1', 'assets/img/brick_01.png');
+    this.load.image('player_1', 'assets/img/Player1.png');
+    this.load.image('player_2', 'assets/img/Player2.png');
+    this.load.image('player_3', 'assets/img/Player3.png');
+    this.load.image('player_4', 'assets/img/Player4.png');
 
 }
 
 function create ()
 {
-
     this.add.image(WIDTH/2, HEIGHT/2, 'bg');
-
 
     //var particles = this.add.particles('brick_01');
 
@@ -49,10 +52,17 @@ function create ()
         //blendMode: 'ADD'
     });
     */
+    var player_x = [PLAYER_OFFSET, WIDTH - PLAYER_OFFSET, WIDTH - PLAYER_OFFSET, PLAYER_OFFSET];
+    var player_y = [HEIGHT - PLAYER_OFFSET, PLAYER_OFFSET, HEIGHT - PLAYER_OFFSET, PLAYER_OFFSET];
+    var player_rot = [45, 225, 315, 135];
 
-    player = this.physics.add.image(50, HEIGHT - 50, 'player').setOrigin(0.5, 1);
-    player.setRotation(0.785);
-    player.setScale(0.2);
+    for (let i = 0; i < MAX_PLAYERS; i++)
+    {
+        let player = this.physics.add.image(player_x[i], player_y[i], 'player_' + (i + 1)).setOrigin(0.5, 1);
+        player.setAngle(player_rot[i]);
+        player.setScale(0.2);
+        players.push(player);
+    }
 
     //.setVelocity(100, 200);
     //.setBounce(1, 1);
@@ -75,16 +85,22 @@ function create ()
 
 function update ()
 {
+    let player1 = players[0];
     if (cursors.left.isDown)
     {
-        player.setAngularVelocity(-TURN_SPEED);
+        player1.setAngularVelocity(-TURN_SPEED);
     }
     else if (cursors.right.isDown)
     {
-        player.setAngularVelocity(TURN_SPEED);
+        player1.setAngularVelocity(TURN_SPEED);
     }
     else
     {
-        player.setAngularVelocity(0);
+        player1.setAngularVelocity(0);
+    }
+
+    if (player1.angle > 10)
+    {
+
     }
 }
